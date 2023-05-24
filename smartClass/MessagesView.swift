ada74dfe25aae53200ad7bb69bbcef41d72e7f97
VadimV3 <myadwordclient@gmail.com>
2023-05-24 21:18:03 +0300

    View finished without footer widget

--- smartClass/MessagesView.swift
+++ smartClass/MessagesView.swift
@@ -8,20 +8,53 @@
 import SwiftUI
 
 struct MessagesView: View {
+    //TODO: - add messages
+    var messages: [String] = ["Test", "Test1", "Test 2"]
+    
     var body: some View {
-        Text("Hello")
+        //title
+        VStack {
+            HStack {
+                Text("Уведомления")
+                    .padding(.leading, 34)
+                    .font(.custom(FontManager.Nunito.bold, size: 32))
+                Spacer()
+            }
+            ScrollView(.vertical) {
+                VStack(spacing: 10) {
+                    ForEach(messages, id: \.self) { messageText in
+                        Message(text: messageText)
+                            .padding(.horizontal, 34)
+                    }
+                }
+            }
+            
+        }
+       
+        
     }
 }
 
+
 // Create message item
-struct Message: View {
+struct Message: View, Identifiable {
+    var id = UUID()
     var text: String
+    
     var body: some View {
-        HStack {
-            Rectangle()
-                .frame(width: 322, height: 80)
-                .backgroundStyle(.gray)
+        HStack() {
+            VStack(alignment: .leading) {
+                Text(text)
+                    .font(.custom(FontManager.Nunito.regular, size: 12))
+                    .padding(.leading, 10)
+                    .padding(.top, 10)
+                Spacer()
+            }
+            Spacer()
         }
+        .frame(width: .infinity, height: 80)
+        .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
+        .cornerRadius(20)
     }
 }
 
