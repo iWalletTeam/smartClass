035c428c1fd69ce99d945c461d3507bda068376e
VadimV3 <myadwordclient@gmail.com>
2023-05-24 19:03:18 +0300

    messages init

---
+++ smartClass/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  smartClass
+//
+//  Created by Vadim on 24.05.2023.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
