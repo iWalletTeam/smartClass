ccafd4a9b0824056e2df34de799d019f1a6888da
volt901 <89735853+volt901@users.noreply.github.com>
2023-05-25 22:27:27 +0500

    авторизация / регистрация макет

---
+++ smartClass/Registration.swift
@@ -0,0 +1,20 @@
+//
+//  Registration.swift
+//  smartClass
+//
+//  Created by Владимир Ширяев on 25.05.2023.
+//
+
+import SwiftUI
+
+struct Registration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Registration_Previews: PreviewProvider {
+    static var previews: some View {
+        Registration()
+    }
+}
