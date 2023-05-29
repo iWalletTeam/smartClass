7cf0d7c71a8a3448222ff3c7952be5f9e70301f1
Vladislav <idevnva@gmail.com>
2023-05-29 19:31:53 +0300

    TabBarView

---
+++ smartClass/View/TabBar/ClockView.swift
@@ -0,0 +1,20 @@
+//
+//  ClockView.swift
+//  smartClass
+//
+//  Created by Vladislav Novoshinskiy on 29.05.2023.
+//
+
+import SwiftUI
+
+struct ClockView: View {
+    var body: some View {
+        Text("Hello, Clock!")
+    }
+}
+
+struct ClockView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClockView()
+    }
+}
---
+++ smartClass/View/TabBar/TabBarView.swift
@@ -0,0 +1,75 @@
+//
+//  TapBarView.swift
+//  smartClass
+//
+//  Created by Vladislav Novoshinskiy on 29.05.2023.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State var selectedTab: TabBarModel = TabBarModel(text: "Календарь", icon: Icons.iconCalendar, tapIcon: Icons.iconTabCalendar)
+    
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                if selectedTab.text == "Календарь" {
+                    CalendarView()
+                } else if selectedTab.text == "Уведомления" {
+                    ClockView()
+                } else if selectedTab.text == "Профиль" {
+                    ProfileView()
+                }
+            }
+            HStack {
+                Spacer()
+                ForEach(TabBarModel.exampleTapBar, id: \.self) { tap in
+                    TapBarItemView(tab: tap.self, selected: $selectedTab)
+                    Spacer()
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.white)
+        }
+    }
+}
+
+struct TapBarItemView: View {
+    @State var tab: TabBarModel
+    @Binding var selected: TabBarModel
+    
+    var body: some View {
+        VStack {
+            Button {
+                withAnimation(.spring()) {
+                    selected = tab
+                }
+            } label: {
+                HStack {
+                    Image(selected.text == tab.text ? tab.tapIcon : tab.icon)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 21, height: 21)
+                    if selected.text == tab.text {
+                        Text(tab.text)
+                            .foregroundColor(Colors.tabIconColor)
+                    }
+                }
+            }
+        }
+        .padding()
+        .background(Colors.buttonTabBG)
+        .clipShape(Capsule())
+    }
+}
+
+struct TapBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
