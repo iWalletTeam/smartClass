632ab8342fd8b9f7c12aa7e584dac40b9e036131
rwrun <islamrun@gmail.com>
2023-05-30 14:33:40 +0500

    add calendar

--- smartClass/ContentView.swift
+++ smartClass/ContentView.swift
@@ -9,14 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                .customFont(font: FontManager.Nunito.black, size: 20)
-        }
-        .padding()
+        CalendarView()
     }
 }
 
---
+++ smartClass/Extension/Color.ext.swift
@@ -0,0 +1,17 @@
+//
+//  Color.ext.swift
+//  smartClass
+//
+//  Created by Ислам Батыргереев on 29.05.2023.
+//
+
+import SwiftUI
+
+extension Color{
+    
+    static var mBlue = Color("Blue")
+    static var mRed = Color("Red")
+    static var mGray = Color("Gray")
+    
+    
+}
