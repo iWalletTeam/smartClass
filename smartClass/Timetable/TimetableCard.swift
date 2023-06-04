930ea1ab2c06c0ba0791ce6afe5bf8d893fa71ee
artemiithefrog <115695294+artemiithefrog@users.noreply.github.com>
2023-06-04 14:55:18 +0200

    Поправил отступы

--- smartClass/Timetable/TimetableCard.swift
+++ smartClass/Timetable/TimetableCard.swift
@@ -15,59 +15,54 @@ struct TimetableCard: View {
     private var audienceNumber = 201
     
     var body: some View {
-//        NavigationView {
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("\(lesson)")
-                        .customFont(font: FontManager.Nunito.bold, size: 16)
-                        .padding(.top, 19)
-                        .padding(.leading, 25)
-                    Spacer()
-                    NavigationLink(destination: TimetableCardFull()) {
-                        Image("arrowIconRight")
-                            .resizable()
-                            .frame(width: 17, height: 17)
-                            .padding(.top, 20)
-                            .padding(.trailing, 25)
-                    }.navigationTitle(Text(""))
-                }
-                Text("\(lessonTime)")
-                    .customFont(font: FontManager.Nunito.regular, size: 12)
-                    .foregroundColor(.white)
-                    .padding(.leading, 10)
-                    .padding(.trailing, 10)
-                    .padding(.top, 3)
-                    .padding(.bottom, 2)
-                    .background(Color("BlueColor"))
-                    .cornerRadius(12)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("\(lesson)")
+                    .customFont(font: FontManager.Nunito.bold, size: 16)
+                    .padding(.top, 19)
                     .padding(.leading, 25)
-                HStack {
-                    Image("buildingIcon")
+                Spacer()
+                NavigationLink(destination: TimetableCardFull()) {
+                    Image("arrowIconRight")
                         .resizable()
-                        .frame(width: 16, height: 16)
-                        .padding(.leading, 25)
-                    Text("корпус: ")
-                        .customFont(font: FontManager.Nunito.regular, size: 14)
-                        .foregroundColor(Color("TextGrayColor"))
-                    Text("\(buildingNumber)")
-                        .customFont(font: FontManager.Nunito.regular, size: 14)
-                        .padding(.leading, -8)
-                    Image("audienceIcon")
-                        .resizable()
-                        .frame(width: 16, height: 16)
-                        .padding(.leading, 25)
-                    Text("аудитория: ")
-                        .customFont(font: FontManager.Nunito.regular, size: 14)
-                        .foregroundColor(Color("TextGrayColor"))
-                    Text("\(audienceNumber)")
-                        .customFont(font: FontManager.Nunito.regular, size: 14)
-                        .padding(.leading, -8)
-                }
-                .padding(.bottom, 33)
+                        .frame(width: 17, height: 17)
+                        .padding(.top, 20)
+                        .padding(.trailing, 25)
+                }.navigationTitle(Text(""))
             }
-            .background(Color("BackgroundGrayColor"))
-            .cornerRadius(20)
-//        }
+            Text("\(lessonTime)")
+                .customFont(font: FontManager.Nunito.regular, size: 12)
+                .foregroundColor(.white)
+                .padding(EdgeInsets(top: 3, leading: 10, bottom: 2, trailing: 10))
+                .background(Color("BlueColor"))
+                .cornerRadius(12)
+                .padding(.leading, 25)
+            HStack {
+                Image("buildingIcon")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .padding(.leading, 25)
+                Text("корпус: ")
+                    .customFont(font: FontManager.Nunito.regular, size: 14)
+                    .foregroundColor(Color("TextGrayColor"))
+                Text("\(buildingNumber)")
+                    .customFont(font: FontManager.Nunito.regular, size: 14)
+                    .padding(.leading, -8)
+                Image("audienceIcon")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .padding(.leading, 25)
+                Text("аудитория: ")
+                    .customFont(font: FontManager.Nunito.regular, size: 14)
+                    .foregroundColor(Color("TextGrayColor"))
+                Text("\(audienceNumber)")
+                    .customFont(font: FontManager.Nunito.regular, size: 14)
+                    .padding(.leading, -8)
+            }
+            .padding(.bottom, 33)
+        }
+        .background(Color("BackgroundGrayColor"))
+        .cornerRadius(20)
         .tint(Color.black)
     }
 }
