fd2d8ea09d76a8b973ef08afba4d3a1abd42ed1e
rwrun <islamrun@gmail.com>
2023-06-19 13:04:19 +0500

    добавил firebase

---
+++ smartClass/Model/FirebaseManager.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseManager.swift
+//  smartClass
+//
+//  Created by Ислам Батыргереев on 19.06.2023.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseManager{
+    
+    //auth
+    
+    //reg
+    
+}
