f6ee61d161a3723038ad133515d59af3e9d87ae0
Aglarov <shamil.aglarov@gmail.com>
2023-06-22 04:17:33 +0300

    Админская часть полностью завершена

--- smartClass/Extension/adminPageView.swift
+++ smartClass/Extension/adminPageView.swift
@@ -4,6 +4,12 @@
 //
 //  Created by Shamil Aglarov on 26.05.2023.
 //
+//
+//  adminPageVF.swift
+//  smartClass
+//
+//  Created by Shamil Aglarov on 26.05.2023.
+//
 
 import SwiftUI
 
@@ -14,69 +20,127 @@ struct Group {
 }
 
 struct AdminView: View {
-
     let groups = [
-        Group(title: "Информационная безопасность", numberOfGroups: 9, numberOfStudents: 101),
-        Group(title: "Компьютерные науки", numberOfGroups: 8, numberOfStudents: 85),
-        Group(title: "Физика", numberOfGroups: 10, numberOfStudents: 120)
+        Group(title: "Информатика и вычислительная техника", numberOfGroups: 9, numberOfStudents: 101),
+        Group(title: "Информатика и вычислительная техника", numberOfGroups: 8, numberOfStudents: 85),
+        Group(title: "Информатика и вычислительная техника", numberOfGroups: 10, numberOfStudents: 120)
     ]
-    
+
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("МГУ. Физ.тех")
-                .font(.largeTitle)
-                .padding(.leading)
-            
-            Text("Группы")
-                .font(.title)
-                .padding([.leading, .bottom])
-            
-            ForEach(groups, id: \.title) { group in
-                ZStack(alignment: .topTrailing) {
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(group.title)
-                                .font(.system(size: 15))
-                                .bold()
-                                .padding(.bottom, 8)
-                            HStack {
-                                Image(systemName: "person.3.fill")
-                                Text("Групп: \(group.numberOfGroups)")
-                                    .font(.headline)
-                                    .bold()
-                                    .foregroundColor(.gray)
-                                Spacer()
-                                Image(systemName: "person.fill")
-                                Text("Студентов: \(group.numberOfStudents)")
-                                    .font(.headline)
-                                    .bold()
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                        .padding()
-                        Spacer()
-                    }
-                    .frame(height: 100)
-                    
-                    Image(systemName: "chevron.right")
-                        .font(.title3)
-                        .foregroundColor(.gray)
-                        .padding([.top, .trailing], 22)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("Название универа")
+                    .font(.system(size: 16, weight: .light))
+                    .padding(.leading, 34)
+                
+                Text("Группы")
+                    .font(.system(size: 32, weight: .bold))
+                    .padding(.leading, 34)
+
+                ForEach(groups, id: \.title) { group in
+                    GroupView(group: group)
                 }
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                .padding([.leading, .trailing])
-                .padding(.bottom,20)
+
+                Spacer(minLength: 8) // Задает минимальное расстояние между предыдущим и следующим элементом
+
+                HStack {
+                    Spacer()
+                    VStack {
+                        Button(action: {
+                            // Add action here
+                        }) {
+                            Image(systemName: "plus")
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding(20)
+                                .background(Color(hex: "#D9D9D8"))
+                                .clipShape(Circle())
+                        }
+                        Text("Добавить группу")
+                            .font(.system(size: 19.36))
+                            .foregroundColor(.black)
+                            .padding(.top, 5) // Изменён отступ сверху до 5
+                            .padding(.bottom, 83)
+                    }
+                    Spacer()
+                }
             }
-            
-            Spacer()
         }
-        .background(Color.white)
+    }
+}
+
+struct GroupView: View {
+    let group: Group
+
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 14) {
+                Text(group.title)
+                    .font(.system(size: 16.3, weight: .bold))
+                    .foregroundColor(.black)
+
+                HStack {
+                    Text("Групп: \(group.numberOfGroups)")
+                        .font(.system(size: 19.36))
+                        .foregroundColor(.black)
+                        .padding(.trailing, 30)
+                    Text("Студентов: \(group.numberOfStudents)")
+                        .font(.system(size: 19.36))
+                        .foregroundColor(.black)
+                }
+            }
+
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray)
+                .alignmentGuide(.top) { d in d[.top] }
+        }
+        .padding([.top, .horizontal], 20)
+        .frame(height: 143)
+        .background(Color(hex: "#D9D9D8"))
+        .cornerRadius(20)
+        .padding(.bottom, 26)
+        .padding(.leading, 34)
+        .padding(.trailing, 29)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AdminView()
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b) = (
+                (int >> 8) * 17,
+                (int >> 4 & 0xF) * 17,
+                (int & 0xF) * 17
+            )
+        case 6: // RGB (24-bit)
+            (r, g, b) = (
+                int >> 16,
+                int >> 8 & 0xFF,
+                int & 0xFF
+            )
+        default:
+            (r, g, b) = (0, 0, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: 1
+        )
     }
 }
